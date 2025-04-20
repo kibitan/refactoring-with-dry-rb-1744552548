56924b438282f8d250822b4d1f701036740e721b
kibitan <uzukifirst@gmail.com>
2025-04-20 21:43:53 +0200

    refactor: 💡 remove unneccesary memoization and put private attr

--- src/task_1/order_processor.rb
+++ src/task_1/order_processor.rb
@@ -8,10 +8,10 @@ class OrderProcessor
   end
 
   def process
-    return Failure(:order_is_nil) if @order.nil?
-    return Failure(:order_must_be_a_hash) unless @order.is_a?(Hash)
-    return Failure(:missing_total_in_order) unless @order.key?(:total)
-    return Failure(:total_must_be_greater_than_zero) if @order[:total].to_f <= 0
+    return Failure(:order_is_nil) if order.nil?
+    return Failure(:order_must_be_a_hash) unless order.is_a?(Hash)
+    return Failure(:missing_total_in_order) unless order.key?(:total)
+    return Failure(:total_must_be_greater_than_zero) if order[:total].to_f <= 0
     return Failure(:final_total_cannot_be_negative) if final_total.negative?
 
     Success(final_total)
@@ -19,16 +19,18 @@ class OrderProcessor
 
   private
 
+  attr_reader :order
+
   def total
-    @total ||= @order[:total].to_f
+    @order[:total].to_f
   end
 
   def shipping_cost
-    @shipping_cost ||= total > 100 ? 10 : 20
+    total > 100 ? 10 : 20
   end
 
   def tax
-    @tax ||= total * 0.1
+    total * 0.1
   end
 
   def gross_total
