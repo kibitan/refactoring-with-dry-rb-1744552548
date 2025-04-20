d7184cbf7d9b26ad2e151f1fa23232c9de764cf2
kibitan <uzukifirst@gmail.com>
2025-04-20 21:38:16 +0200

    refactor: 💡 put values to private method

--- src/task_1/order_processor.rb
+++ src/task_1/order_processor.rb
@@ -12,20 +12,34 @@ class OrderProcessor
     return Failure(:order_must_be_a_hash) unless @order.is_a?(Hash)
     return Failure(:missing_total_in_order) unless @order.has_key?(:total)
     return Failure(:total_must_be_greater_than_zero) if @order[:total].to_f <= 0
-
-    total = @order[:total].to_f
-    shipping_cost = if total > 100
-                      10
-                    else
-                      20
-                    end
-    tax = total * 0.1
-    gross_total = total + shipping_cost + tax
-    discount = 0
-    discount = 50 if gross_total > 1000
-    final_total = gross_total - discount
     return Failure(:final_total_cannot_be_negative) if final_total < 0
 
     Success(final_total)
+  end
+
+  private
+
+  def total
+    @order[:total].to_f
+  end
+
+  def shipping_cost
+    total > 100 ? 10 : 20
+  end
+
+  def tax
+    total * 0.1
+  end
+
+  def gross_total
+    total + shipping_cost + tax
+  end
+
+  def discount
+    gross_total > 1000 ? 50 : 0
+  end
+
+  def final_total
+    gross_total - discount
   end
 end
