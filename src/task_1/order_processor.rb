5b349f750e871eede12a519bbdaf6fc504bdc855
kibitan <uzukifirst@gmail.com>
2025-04-20 21:52:53 +0200

    refactor: 💡 use bind instead of return

--- src/task_1/order_processor.rb
+++ src/task_1/order_processor.rb
@@ -8,37 +8,32 @@ class OrderProcessor
   end
 
   def process
-    return Failure(:order_is_nil) if order.nil?
-    return Failure(:order_must_be_a_hash) unless order.is_a?(Hash)
-    return Failure(:missing_total_in_order) unless order.key?(:total)
-    return Failure(:total_must_be_greater_than_zero) if order[:total].to_f <= 0
-    return Failure(:final_total_cannot_be_negative) if final_total.negative?
-
-    Success(final_total)
+    find_total(@order).bind do |total|
+      calculate_final_total(total)
+    end
   end
 
   private
 
-  attr_reader :order
+  def find_total(order)
+    return Failure(:order_is_nil) if order.nil?
+    return Failure(:order_must_be_a_hash) unless order.is_a?(Hash)
+    return Failure(:missing_total_in_order) unless order.key?(:total)
+    return Failure(:total_must_be_greater_than_zero) if order[:total].to_f <= 0
 
-  def total
-    @order[:total].to_f
+    Success(order[:total].to_f)
   end
 
-  def shipping_cost
-    total > 100 ? 10 : 20
-  end
+  def calculate_final_total(total)
+    shipping_cost = total > 100 ? 10 : 20
+    tax = total * 0.1
+    gross_total = total + shipping_cost + tax
+    discount = gross_total > 1000 ? 50 : 0
 
-  def tax
-    total * 0.1
-  end
+    final_total = gross_total - discount
+    return Failure(:final_total_cannot_be_negative) if final_total.negative?
 
-  def gross_total
-    @gross_total ||= total + shipping_cost + tax
-  end
-
-  def discount
-    @discount ||= gross_total > 1000 ? 50 : 0
+    Success(final_total)
   end
 
   def final_total
